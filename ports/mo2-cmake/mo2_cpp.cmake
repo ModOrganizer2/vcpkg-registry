c9f971437e00a3a4d77c22654eb046923ecbd1fb
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-08 16:58:42 +0200

    [mo2-cmake] Remove .natvis inclusion.

--- ports/mo2-cmake/mo2_cpp.cmake
+++ ports/mo2-cmake/mo2_cpp.cmake
@@ -111,7 +111,6 @@ function(mo2_configure_target TARGET)
 	file(GLOB_RECURSE ui_files CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/*.ui)
 	file(GLOB_RECURSE ui_header_files CONFIGURE_DEPENDS ${UI_HEADERS_DIR}/*.h)
 	file(GLOB_RECURSE rule_files CONFIGURE_DEPENDS ${CMAKE_BINARY_DIR}/*.rule)
-	file(GLOB_RECURSE misc_files CONFIGURE_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/../*.natvis)
 
 	if (${MO2_SOURCE_TREE})
 		source_group(TREE ${CMAKE_CURRENT_SOURCE_DIR}
