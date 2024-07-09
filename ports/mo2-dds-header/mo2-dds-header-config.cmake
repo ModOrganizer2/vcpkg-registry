7298336eef3131bd070822c49b6cc4fc4bafbcd8
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-09 19:25:46 +0200

    [mo2-dds-header] Fix imported target.

--- ports/mo2-dds-header/mo2-dds-header-config.cmake
+++ ports/mo2-dds-header/mo2-dds-header-config.cmake
@@ -6,5 +6,5 @@ get_filename_component(_IMPORT_PREFIX "${CMAKE_CURRENT_LIST_FILE}" PATH)
 get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 get_filename_component(_IMPORT_PREFIX "${_IMPORT_PREFIX}" PATH)
 
-add_library(mo2::dds-header IMPORTED)
+add_library(mo2::dds-header INTERFACE IMPORTED)
 target_sources(mo2::dds-header INTERFACE ${_IMPORT_PREFIX}/include/DDS.h)
