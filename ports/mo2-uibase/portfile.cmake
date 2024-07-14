8b5c6a45d63a5902f1912737091afe3f3590c189
Mikaël Capelle <capelle.mikael@gmail.com>
2024-07-14 09:50:14 +0200

    [mo2-uibase] Many minor fixes for port.

--- ports/mo2-uibase/portfile.cmake
+++ ports/mo2-uibase/portfile.cmake
@@ -1,3 +1,10 @@
+# uibase is prebuilt and is always dynamic, but MO2 uses a static triplets (for Boost,
+# lz4, etc.) so we disable that warning
+set(VCPKG_POLICY_DLLS_IN_STATIC_LIBRARY enabled)
+
+# TODO: add a copyright file to uibase and install it
+set(VCPKG_POLICY_SKIP_COPYRIGHT_CHECK enabled)
+
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/ModOrganizer2/modorganizer-uibase/releases/download/v${VERSION}/uibase_v${VERSION}.7z"
     FILENAME "uibase_${VERSION}.7z"
@@ -11,6 +18,30 @@ vcpkg_extract_source_archive(
 )
 
 file(INSTALL ${SOURCE_PATH}/include DESTINATION ${CURRENT_PACKAGES_DIR})
-file(INSTALL ${SOURCE_PATH}/bin DESTINATION ${CURRENT_PACKAGES_DIR})
-file(INSTALL ${SOURCE_PATH}/lib DESTINATION ${CURRENT_PACKAGES_DIR})
-file(INSTALL ${SOURCE_PATH}/pdb DESTINATION ${CURRENT_PACKAGES_DIR})
+
+file(INSTALL
+    ${SOURCE_PATH}/bin/uibase.dll
+    ${SOURCE_PATH}/pdb/uibase.pdb
+DESTINATION
+    ${CURRENT_PACKAGES_DIR}/bin
+)
+file(INSTALL
+    ${SOURCE_PATH}/lib/uibase.lib
+    ${SOURCE_PATH}/lib/cmake
+DESTINATION
+    ${CURRENT_PACKAGES_DIR}/lib
+)
+
+file(INSTALL
+    ${SOURCE_PATH}/bin/uibased.dll
+    ${SOURCE_PATH}/pdb/uibased.pdb
+DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(INSTALL
+    ${SOURCE_PATH}/lib/uibased.lib
+    ${SOURCE_PATH}/lib/cmake
+DESTINATION
+    ${CURRENT_PACKAGES_DIR}/debug/lib
+)
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME "mo2-uibase" CONFIG_PATH "lib/cmake/mo2-uibase")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
